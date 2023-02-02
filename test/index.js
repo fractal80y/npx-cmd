0821db78fc4fcc4c7e8782210264fd532f847a65
Matt Zvirbulis <30788255+fractal80y@users.noreply.github.com>
2023-02-02 13:19:18 +1100

    init

---
+++ test/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+console.log("hello i matthew created this npx comand")
+console.log("run npm i -g .")
+console.log("this will install package and will be callable from shell")
+console.log("run npm rm -g .") 
+console.log("this will install undo and remove it")
